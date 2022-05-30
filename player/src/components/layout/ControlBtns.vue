2f80628a61ea7210ea7c1a6eff21aae74031ee25
YANNのLAPTOP\yann <yann_xu@outlook.com>
2022-05-30 18:17:40 +0800

    ✨ feat: add header & controlBtns components

--- player/src/components/layout/ControlBtns.vue
+++ player/src/components/layout/ControlBtns.vue
@@ -1,13 +1,73 @@
 <script setup lang="ts">
-
+import { ref, reactive} from 'vue'
+import {
+  PauseOutlined,
+  StepBackwardOutlined,
+  StepForwardOutlined,
+  CaretRightOutlined,
+} from '@ant-design/icons-vue'
+const progress = ref(30)
+const strokeColor = ref('#000')
+const isPlaying = ref(false)
+const songInfo = reactive({
+  cover: `/src/assets/avatar.png`,
+  name: '--',
+  singer: '--',
+  duration: '04:00'
+})
+const iconFontSize = reactive({
+  fontSize: '26px'
+})
 </script>
 
 <template>
-  <div class="footer bg-white">btns</div>
+  <div class="footer bg-white flex justify-between items-center">
+    <div class="song-info flex">
+      <div class="song-cover mr-2">
+        <img :src="songInfo.cover" />
+      </div>
+      <div>
+        <div class="song">{{songInfo.name}}</div>
+        <div class="singer">{{songInfo.singer}}</div>
+      </div>
+    </div>
+    <div class="player-control">
+      <div class="player-control-btns flex justify-around items-center">
+        <StepBackwardOutlined :style="iconFontSize"/>
+        <span> <PauseOutlined v-if="isPlaying" :style="iconFontSize" /><CaretRightOutlined  :style="iconFontSize" v-else /></span>
+        <StepForwardOutlined :style="iconFontSize" />
+      </div>
+      <div class="player-control-slider flex justify-around items-center">
+        <span class="pr-2">00:00</span>
+        <a-progress :percent="progress" :stroke-color="strokeColor" :showInfo="false" />
+        <span class="pl-2">{{songInfo.duration}}</span>
+      </div>
+    </div>
+    <div class="other-control">
+
+    </div>
+  </div>
 </template>
 
 <style lang="scss" scoped>
 .footer {
   height: 50px;
+  padding: 0 50px;
+  .song-cover {
+    width: 40px;
+    height: 40px;
+    border-radius: 4px;
+    img {
+      width: 100%;
+      height: 100%;
+      border-radius: 4px;
+    }
+  }
+  .player-control {
+    width: 200px;
+  }
+  .other-control {
+    width: 100px;
+  }
 }
 </style>
--- player/src/components/layout/Header.vue
+++ player/src/components/layout/Header.vue
@@ -1,14 +1,47 @@
 <script setup lang="ts">
   // import {reactive} from 'vue'
+  import { useMainStore } from '@/store'
+
+  const store = useMainStore()
+  const userProfile = store.getProfile
+  console.log(userProfile)
+  const account = store.getAccount
+  console.log(account)
   
 </script>
 
 <template>
-  <div class="header bg-white">header</div>
+  <div class="header bg-white flex justify-between items-center">
+    <div><img class="logo" src="../../assets/logo.png" alt=""></div>
+    <div>
+      <a-dropdown>
+        <a @click.prevent class="flex items-center"><img class="avatar" :src="userProfile.avatarUrl">{{userProfile.nickname}}</a>
+        <template #overlay>
+          <a-menu>
+            <a-menu-item key="0">
+              <a href="">个人中心</a>
+            </a-menu-item>
+            <a-menu-item key="1">
+              <a href="">退出</a>
+            </a-menu-item>
+          </a-menu>
+        </template>
+      </a-dropdown>      
+    </div>
+  </div>
 </template>
 
 <style lang="scss" scoped>
 .header {
   height: 50px;
+  padding: 0 50px;
+  .logo {
+    height: 40px;
+  }
+  .avatar {
+    width: 40px;
+    height: 40px;
+    border-radius: 50%;
+  }
 }
 </style>
