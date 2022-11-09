9a2bc55d3b7ebd26a5e0ba4ad042d8be09488ab3
yann <yann_xu@outlook.com>
2022-11-09 15:51:05 +0800

    ✨feat: add windicss

--- player/vite.config.ts
+++ player/vite.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
+import WindiCSS from 'vite-plugin-windicss'
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -13,5 +15,5 @@ export default defineConfig({
       '#': path.resolve(__dirname, 'types'),
     },
   },
-  plugins: [vue()]
+  plugins: [WindiCSS(), vue()]
 })
